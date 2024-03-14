ab910e7e6ff07b366c1d3bfb140d27eba2e194dd
juliapsnyder <juliapsnyder@users.noreply.github.com>
2024-03-14 14:41:56 -0400

    added do-stats.sh

---
+++ do-stats.sh
@@ -0,0 +1,10 @@
+# This should be run in a directory with
+# files that start with NENE*txt
+# NENE* files should contain lists of
+# numbers, one per line
+
+for file in NENE*A.txt NENE*B.txt
+do
+   echo $file
+   bash goostats.sh $file stat-$file
+done
